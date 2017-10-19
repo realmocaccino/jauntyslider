bad93784e6996b0fe383d3caf6a44eaabff3f917
Luiz Gustavo Martins <contato@luizgustavoweb.com>
2017-10-19 00:07:59 -0200

    Loop with ecmascript 6

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -2,7 +2,7 @@ exports.get_options = function(element) {
 	var options = {};
 	var data = element.dataset.jauntyslider.replace(/\s+/g, '').split(';');
 	
-	for(i in data) {
+	for(let i of data) {
 		if(data[i]) {
 			options[data[i].split(':')[0].toLowerCase()] = data[i].split(':')[1].toLowerCase();
 		}
