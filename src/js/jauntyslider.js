eefc53f501e21f0acc7712146b8c6ff8eb4d8fb4
Luiz Gustavo Martins <contato@luizgustavoweb.com>
2017-11-07 21:48:30 -0200

    Update Current Navigation method

--- src/js/jauntyslider.js
+++ src/js/jauntyslider.js
@@ -215,21 +215,20 @@ module.exports = function(userOptions)
 	};
 
 	this.previousSlide = function() {
-		if((this.auxiliaries.currentSlide-this.step) >= 0 || this.loop) {
-			if(((this.auxiliaries.currentSlide == 0) || (this.auxiliaries.currentSlide-this.step) < 0) && this.loop) {
+		if((this.auxiliaries.currentSlide - this.options.step) >= 0 || this.options.loop) {
+			if(((this.auxiliaries.currentSlide == 0) || (this.auxiliaries.currentSlide - this.options.step) < 0) && this.options.loop) {
 				this.auxiliaries.currentSlide = this.auxiliaries.totalSlides-1;
 				//this.move(this.auxiliaries.slidesPositions[this.auxiliaries.currentSlide], 1);
 			} else {
-				this.auxiliaries.currentSlide -= this.step;
+				this.auxiliaries.currentSlide -= this.options.step;
 				//this.move(this.auxiliaries.slidesPositions[this.auxiliaries.currentSlide]);
 			}
-			this.navigation.find('.'+this.auxiliaries.labelActive).removeClass(this.auxiliaries.labelActive);
-			this.navigationItems.eq(this.auxiliaries.currentSlide).addClass(this.auxiliaries.labelActive);
+			this.updateCurrentNavigationItem();
 		}
 	};
 
 	this.nextSlide = function() {
-		if((this.auxiliaries.currentSlide + this.options.step) <= (this.auxiliaries.totalSlides - 1) || this.options.loop) {
+		if((this.auxiliaries.currentSlide + this.options.step) < this.auxiliaries.totalSlides || this.options.loop) {
 			if((this.auxiliaries.currentSlide == (this.auxiliaries.totalSlides - 1) || (this.auxiliaries.currentSlide + this.options.step) > (this.auxiliaries.totalSlides - 1)) && this.options.loop) {
 				this.auxiliaries.currentSlide = 0;
 				//this.move(this.auxiliaries.slidesPositions[this.auxiliaries.currentSlide], 1);
@@ -237,9 +236,8 @@ module.exports = function(userOptions)
 				this.auxiliaries.currentSlide += this.options.step;
 				//this.move(this.auxiliaries.slidesPositions[this.auxiliaries.currentSlide]);
 			}
-			this.navigation.find('.'+this.auxiliaries.labelActive).removeClass(this.auxiliaries.labelActive);
-			this.navigationItems.eq(this.auxiliaries.currentSlide).addClass(this.auxiliaries.labelActive);
-		} else if(this.slideshow) {
+			this.updateCurrentNavigationItem();
+		} else if(this.options.slideshow) {
 			this.stopSlideshow();
 		}
 	};
@@ -274,6 +272,11 @@ module.exports = function(userOptions)
 		if(this.auxiliaries.currentSlide != (this.auxiliaries.totalSlides-1)) {
 			this.nextArrow.css('visibility','visible');
 		}
+	};
+	
+	this.updateCurrentNavigationItem = function() {
+		this.elements.navigation.querySelector('.' + this.auxiliaries.labelActive).classList.remove(this.auxiliaries.labelActive);
+		this.elements.navigationItems.item(this.auxiliaries.currentSlide).classList.add(this.auxiliaries.labelActive);
 	};
 	
 	this.startSlideshow = function() {
