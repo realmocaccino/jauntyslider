4853957950ede0ec3369805aa7342715e4ce21fa
Luiz Gustavo Martins <contato@luizgustavoweb.com>
2017-11-06 22:07:26 -0200

    Fix event listener elements

--- src/js/jauntyslider.js
+++ src/js/jauntyslider.js
@@ -190,25 +190,27 @@ module.exports = function(userOptions)
 	};
 	
 	this.actions = function() {
-		this.previousArrow.addEventListener('click', function(event) {
+		this.elements.previousArrow.addEventListener('click', function(event) {
 			event.preventDefault();
 			
 			this.previousSlide();
 			if(this.slideshow) this.restartSlideshow();
 		});
 		
-		this.nextArrow.addEventListener('click', function(event) {
+		this.elements.nextArrow.addEventListener('click', function(event) {
 			event.preventDefault();
 		
 			this.nextSlide();
 			if(this.slideshow) this.restartSlideshow();
 		});
 		
-		this.navigationSlides.addEventListener('click', function(event) {
-			event.preventDefault();
+		this.elements.navigationSlides.forEach(function(slide) {
+			slide.addEventListener('click', function(event) {
+				event.preventDefault();
 			
-			this.navigate(this);
-			if(this.slideshow) this.restartSlideshow();
+				this.navigate(this);
+				if(this.slideshow) this.restartSlideshow();
+			});
 		});
 	};
 
