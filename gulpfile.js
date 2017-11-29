4701fb54c2e1366c6510e674e3f2ca86561e65e7
Luiz Gustavo Martins <lgustavoms@gmail.com>
2017-11-29 15:35:01 -0200

    Run default task before watch

--- gulpfile.js
+++ gulpfile.js
@@ -34,6 +34,6 @@ gulp.task('images', function(){
 
 gulp.task('default', ['styles', 'scripts', 'images']);
 
-gulp.task('watch', function(){
+gulp.task('watch', ['default'], function(){
 	gulp.watch([srcStyles, srcScripts, srcImages], ['default']);
 });
