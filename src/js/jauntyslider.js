430ef3e0fef29df2ec08cc499dba1981b9e81f5e
Luiz Gustavo Martins <contato@luizgustavoweb.com>
2017-12-04 21:54:53 -0200

    Get first list item to determine slider height

--- src/js/jauntyslider.js
+++ src/js/jauntyslider.js
@@ -107,7 +107,7 @@ module.exports = function(user_options)
 	};
 	
 	this.setHeight = function() {
-		if(!this.userOptions.height) this.options.height = this.elements.list.offsetHeight;
+		if(!this.userOptions.height) this.options.height = this.elements.list.offsetHeight || this.elements.slides[0].offsetHeight;
 	};
 	
 	this.setInitialSlide = function() {
@@ -153,8 +153,9 @@ module.exports = function(user_options)
 		
 		this.elements.wrapper.style.setProperty('width', this.concatenateUnit(this.options.width));
 		this.elements.wrapper.style.setProperty('height', this.concatenateUnit(this.options.height));
-		
+
 		this.elements.list.style.setProperty('width', this.concatenateUnit(this.auxiliaries.listWidth), 'important');
+		this.elements.list.style.setProperty('height', this.concatenateUnit(this.options.height));
 		
 		this.move(true);
 		
