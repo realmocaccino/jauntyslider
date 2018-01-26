9d47cfacc500e119dd2b85d48dfb30b05c5d0824
Luiz Gustavo Martins <contato@luizgustavoweb.com>
2018-01-25 22:27:45 -0200

    No need to set enumerable when prototyping

--- src/js/init.js
+++ src/js/init.js
@@ -1,8 +1,7 @@
-Object.defineProperty(Object.prototype, 'jauntyslider', {
-	enumerable: false,
-	value: require('./jauntyslider.js')
-});
 const helpers = require('./helpers.js');
+const jauntyslider = require('./jauntyslider.js');
+
+HTMLElement.prototype.jauntyslider = jauntyslider;
 
 document.addEventListener('DOMContentLoaded', () => {
 	Array.from(document.querySelectorAll('ul[data-jauntyslider]')).forEach(list => {
