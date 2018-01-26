1ef53b01d96f48c1ae75765efa27e198e0dba029
Luiz Gustavo Martins <lgustavoms@gmail.com>
2018-01-26 13:59:50 -0200

    Add support querySelectorAll

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,15 @@
 import * as helpers from './helpers.js';
 import { jauntyslider } from './jauntyslider.js';
 
+// From querySelector
 HTMLElement.prototype.jauntyslider = jauntyslider;
 
+// From querySelectorAll
+NodeList.prototype.jauntyslider = function(options) {
+	Array.from(this).forEach(list => list.jauntyslider(options));
+};
+
+// From data-jauntyslider
 document.addEventListener('DOMContentLoaded', () => {
 	Array.from(document.querySelectorAll('ul[data-jauntyslider]')).forEach(list => {
 		list.jauntyslider(helpers.getOptions(list));
