06b3af5d22650f59f03ddce27dd23bac96cc3bfc
Luiz Gustavo Martins <lgustavoms@gmail.com>
2023-03-20 01:57:52 -0300

    Remove semicolons

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,24 @@
-var gulp = require('gulp');
-var babel = require('gulp-babel');
-var concat = require('gulp-concat');
-var minifyCss = require('gulp-minify-css');
-var sass = require('gulp-sass');
-var uglify = require('gulp-uglify');
-var webpack = require('webpack-stream');
+var babel = require('gulp-babel')
+var concat = require('gulp-concat')
+var gulp = require('gulp')
+var minifyCss = require('gulp-minify-css')
+var sass = require('gulp-sass')
+var uglify = require('gulp-uglify')
+var webpack = require('webpack-stream')
 
-var srcStyles = 'src/sass/**/*.scss';
-var srcScripts = 'src/js/**/*.js';
+var srcStyles = 'src/sass/**/*.scss'
+var srcScripts = 'src/js/**/*.js'
 
-var distStyles = 'dist/css';
-var distScripts = 'dist/js';
+var distStyles = 'dist/css'
+var distScripts = 'dist/js'
 
 gulp.task('styles', function(){
 	return gulp.src(srcStyles)
 	.pipe(concat('jauntyslider.min.css'))
 	.pipe(sass())
 	.pipe(minifyCss())
-	.pipe(gulp.dest(distStyles));
-});
+	.pipe(gulp.dest(distStyles))
+})
 
 gulp.task('scripts', function(){
 	return gulp.src(srcScripts)
@@ -26,11 +26,11 @@ gulp.task('scripts', function(){
 	.pipe(concat('jauntyslider.min.js'))
 	.pipe(babel())
 	.pipe(uglify())
-	.pipe(gulp.dest(distScripts));
-});
+	.pipe(gulp.dest(distScripts))
+})
 
-gulp.task('default', ['styles', 'scripts']);
+gulp.task('default', ['styles', 'scripts'])
 
 gulp.task('watch', ['default'], function(){
-	gulp.watch([srcStyles, srcScripts], ['default']);
-});
+	gulp.watch([srcStyles, srcScripts], ['default'])
+})
