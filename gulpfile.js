1b53f42f5c082ab29976745d39f355adafa452ae
Luiz Gustavo Martins <lgustavoms@gmail.com>
2023-03-20 01:55:49 -0300

    Arrows are created from CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,6 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
-var changed = require('gulp-changed');
 var concat = require('gulp-concat');
-var image = require('gulp-image');
 var minifyCss = require('gulp-minify-css');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
@@ -10,11 +8,9 @@ var webpack = require('webpack-stream');
 
 var srcStyles = 'src/sass/**/*.scss';
 var srcScripts = 'src/js/**/*.js';
-var srcImages = 'src/img/**/*.*';
 
 var distStyles = 'dist/css';
 var distScripts = 'dist/js';
-var distImages = 'dist/img';
 
 gulp.task('styles', function(){
 	return gulp.src(srcStyles)
@@ -33,15 +29,8 @@ gulp.task('scripts', function(){
 	.pipe(gulp.dest(distScripts));
 });
 
-gulp.task('images', function(){
-	return gulp.src(srcImages)
-	.pipe(changed(distImages))
-	.pipe(image())
-	.pipe(gulp.dest(distImages));
-});
-
-gulp.task('default', ['styles', 'scripts', 'images']);
+gulp.task('default', ['styles', 'scripts']);
 
 gulp.task('watch', ['default'], function(){
-	gulp.watch([srcStyles, srcScripts, srcImages], ['default']);
+	gulp.watch([srcStyles, srcScripts], ['default']);
 });
